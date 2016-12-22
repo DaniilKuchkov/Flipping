0e5ae5c2b3f1411e45d6ed36865e4319252c0abf
Segey Panferov <panferov.serf@gmail.com>
2016-12-22 18:22:42 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="">
+    <title>Serf.ai</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!--<script>-->
+    <!--    $(document).ready(function(){-->
+    <!--        $('.mini').animate(all, 3, easein)-->
+    <!--    }-->
+    <!--</script>-->
+</head>
+
+<body>
+<!-- ********** HEADER ********** -->
+<div class="demo-1">
+    <div class="content">
+        <div id="large-header" class="large-header">
+            <canvas id="demo-canvas"></canvas>
+            <div class="logo"><img src="img/logo.png"></div>
+            <h1 class="main-title">Serf <span class="thin">Artificial Intelegence</span></h1>
+            <p class="main-tagline">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>
+                Fusce vel dictumarcu. Praesent et maximus libero.<br/>
+                Lorem ipsum dolor sit amet,consectetur adipiscing elit<br><br>
+                <button class="btn btn-conf btn-theme">Learn More</button>
+            </p>
+        </div>
+    </div>
+</div>
+<!--/header-->
+<div id="blk">
+    <div class="container">
+        <div class="row">
+            <h3 class="centered">What is SERF</h3>
+            <div class="col-md-4 col-md-offset-1">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book.</p>
+            </div>
+            <div class="col-md-4 col-md-offset-2">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="technology">
+    <div class="container">
+        <h3 class="centered">Technology</h3><br/><br/>
+        <table class="table">
+            <tr>
+                <td></td>
+                <td>Speech to Speech <br/>Translation</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td class='td1'>Computer Vision <br/> Image Recognition <br/>(Applications)</td>
+                <td rowspan="6" class="td2" id="mini">
+                        <div class="mini">
+
+                            <div class="star">
+
+                            </div>
+                            <canvas id="demo2-canvas">
+                            </canvas>
+                        </div>
+
+
+                </td>
+                <td class="td3">Deep Learning <br/>Machine Learning <br/>(Platforms)</td>
+            </tr>
+            <tr>
+                <td>
+                    <div>Computer Vision <br/> Image Recognition <br/>(Platforms)</span></div>
+                </td>
+                <td>Gesture <br/>Control</td>
+            </tr>
+            <tr>
+                <td>Context Aware<br/>Computing</td>
+
+                <td>Virtual Assistans</td>
+            </tr>
+            <tr>
+                <td>Deep Learning <br/> Machine Learning <br/>(Applications)</td>
+
+                <td>NLP <br/>Text mining</td>
+            </tr>
+            <tr>
+                <td>Smart Robots</td>
+
+                <td>Natural Language <br/>Processing</td>
+            </tr>
+            <tr>
+                <td class="td1">Video Automatic <br/> Content Recognition</td>
+                <td class="td3">Personalized<br/> Recommendation<br/>Engines</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>Speech Recognition</td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</div>
+<div id="some">
+    <div class="container">
+        <h3 class="centered">Spheres where Al can be used</h3>
+        <h3 class="centered">Portfolio</h3>
+        <h3 class="centered">Reviews</h3>
+        <h3 class="centered">One more block</h3>
+    </div>
+</div>
+<div id="trailer">
+    <video id="video" width="100%" height="auto" autoplay="autoplay" loop="loop" preload="auto">
+        <source src="video/space.mp4"></source>
+        <source src="" type="video/webm"></source>
+    </video>
+    <!--        <div class="centered article">Get in touch with us</div>-->
+
+</div>
+
+<!--<script>-->
+<!--    var img1 = document.getElementById("img1");-->
+<!--    var pic = new Image();-->
+<!--    pic.src = "img/1.png";-->
+<!--    canvas = document.getElementById('pics');-->
+<!--    ctx = canvas.getContext('2d');-->
+<!--    pic.onload = function () {-->
+<!--        ctx.drawImage(pic, 0, 0);-->
+<!--    }-->
+<!--    ctx = canvas.getContext('2d');-->
+<!--</script>-->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/TweenLite.min.js"></script>
+<script src="js/EasePack.min.js"></script>
+<script src="js/demo-1.js"></script>
+<script src="js/demo-2.js"></script>
+<!--<script src="js/star.js"></script>-->
+</body>
+</html>
