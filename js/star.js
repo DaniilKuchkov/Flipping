d29ee0489891afb76e9497fa134a6f11e1991a57
DevFizzah <kuchkov.daniil@gmail.com>
2017-01-17 18:26:20 +0200

    3D cube

--- js/star.js
+++ js/star.js
@@ -1,37 +1,75 @@
-var sphere = document.getElementById('sphere');
-var stars = document.getElementsByClassName('star');
-var tech = document.getElementsByClassName('technology-part');
-
-sphere.addEventListener('mouseover',function(e){
-		var target = e.target;
-		if(target.className == 'star' && target.dataset.num != 'all'){
-			target.style.transform = 'scale(1.7)';
-			target.style.boxShadow = '0px 0px 21px 20px rgba(0,235,252,1);';
-			tech[+target.dataset.num].classList.add('active');
-			setTimeout(function(){
-				tech[+target.dataset.num].classList.remove('active');
-				target.style.transform = 'scale(1.0)';
-			},1000);
-		}
-		if(target.dataset.num == 'all') {
-			for(var i = 0; i < tech.length;i++){
-				tech[i].classList.add('active');
-				setTimeout(function(){
-					console.log(tech[i]);
-					tech[i].classList.remove('active');
-				},4000);
-			}
-		};
-});
-var num = 14;
-var wrap = 300;
-var radius = 290;
-
 $(document).ready(function() {
-  for (i=0;i<num; i++){
-    var f = 2 / num * i * Math.PI;
+var sphere = $('#sphere');
+var tech = $('.technology-part');
+function clearClass(arr,cls){
+	for(var i = 0; i < arr.length;i++){
+		arr[i].classList.remove(cls);
+	}
+}
+    $(".arrow").bind('click', function(e){
+        e.preventDefault();
+        $('body,html').animate({scrollTop: document.documentElement.scrollHeight},5000, "linear");
+    });
+function circlePleaced(num,wrap,radius){
+	for (i=0;i<num; i++){
+    var f = 2/num * i * Math.PI;
     var left = wrap + radius * Math.sin(f) + 'px';
     var top = wrap + radius * Math.cos(f) + 'px';
     $('.technology-part').eq(i).css({'top':top,'left':left});
     }
+}
+/*$('#front-right').hover(function(){
+	$('.flipper').toggleClass('angle-negative');
 });
+$('#front-left').hover(function(){
+	$('.flipper').toggleClass('angle-positive');
+});
+$('#front-right').click(function(){
+	$('.flipper').addClass('flip-right');
+});
+$('#front-left').click(function(){
+	$('.flipper').addClass('flip-left');
+});*/
+$('.star').hover(function(){
+	var self = this;
+		if(this.dataset.num != 'all'){
+			this.style.transform = 'scale(1.7)';
+			this.style.boxShadow = '0px 0px 21px 20px rgba(0,235,252,1);';
+			tech[+this.dataset.num].classList.add('active');
+			setTimeout(function(){
+				tech[+self.dataset.num].classList.remove('active');
+				self.style.transform = 'scale(1.0)';
+			},2000);
+		}
+		if(this.dataset.num == 'all') {
+			for(var i = 0; i < tech.length;i++){
+				tech[i].classList.add('active');
+			}
+			setTimeout(function(){
+					clearClass(tech,'active');
+				},4000);
+		};
+});
+
+  circlePleaced(14,300,300);
+});
+var flipper = document.querySelector('.flipper');
+var divInFlipper = document.querySelectorAll('.flipper > div');
+var panelSize = divInFlipper[0].clientWidth;
+var angle = 360/divInFlipper.length;
+var curAngle = 0;
+var tz = Math.round( ( panelSize / 2 ) / Math.tan( Math.PI / divInFlipper.length ) );
+for(var i = 0; i < divInFlipper.length;i++){
+		divInFlipper[i].style.transform = 'rotateY( ' + curAngle + 'deg )' + ' translateZ( ' + tz + 'px )';
+		curAngle+= angle;
+}
+var s = 0;
+flipper.addEventListener('click',function(e){
+	var target = e.target;
+	console.log(target.tagName);
+	if(target.tagName == 'DIV'){
+		s-= angle;
+		this.style.transform = 'rotateY( ' + s + 'deg )';
+	}
+});
+
